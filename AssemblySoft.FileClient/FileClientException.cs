0d3f1295705510485c3b0b93e48e8e7f02ae3d87
lv31487 <carl.randall@lv.com>
2017-04-26 12:37:04 +0100

    Add project files.

---
+++ AssemblySoft.FileClient/FileClientException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AssemblySoft.FileClient
+{
+    /// <summary>
+    /// File client exception
+    /// </summary>
+    class FileClientException : Exception
+    {
+        public FileClientException() {}
+
+        public FileClientException(string message):base(message)
+        {
+        }
+
+        public FileClientException(string message,Exception inner):base(message,inner)
+        {
+        }
+    }
+}
