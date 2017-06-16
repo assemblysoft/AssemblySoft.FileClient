e08c57f6f7e3d09ff4e4d056aeb54a9d54edf6e4
Carl Randall <carl@assemblysoft.com>
2017-06-16 14:45:32 +0100

    more echo statements

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 ::@echo Off
+
 echo start custom build
 
 set config=%1
@@ -16,8 +17,10 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+echo building solution
 "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" AssemblySoft.FileClient.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
+echo creating nuget package
 mkdir Build
 call %nuget% pack "AssemblySoft.FileClient\AssemblySoft.FileClient.csproj" -IncludeReferencedProjects -verbosity detailed -o Build -p Configuration=%config% %version%
 
